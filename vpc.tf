e7051e52499147e7ca1dabf2268c9f6125d97b41
iron87 <fede.cerruto@gmail.comm>
2022-10-20 16:49:08 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,42 +1,15 @@
 module "network" {
   source  = "app.terraform.io/federicocerruto-training/network/google"
   version = "3.4.0"
-  # insert required variables here
-}
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
 
+  # insert required variables here
     project_id   = var.project
     network_name = "example-vpc"
-    routing_mode = "GLOBAL"
-
-    subnets = [
+        subnets = [
   {
     subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
-
-    secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
-
-        subnet-02 = []
-    }
-
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        }
-    ]
 }
